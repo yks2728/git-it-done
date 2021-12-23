0c2b87fddf225c96448e4ae83ad0ce1627f75b81
Zack Greenfield <zackg007@verizon.net>
2021-12-23 08:22:14 -0500

    updated files for query parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,22 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    // grab repo name from url query strings
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     // format the github api url
@@ -18,10 +34,9 @@ var getRepoIssues = function(repo) {
                     displayWarning(repo);
                 }
             });
-        }
-        else {
-            console.log(response);
-            alert("There was a problem with your request!");
+        } else {
+           // if not successful, redirect to homepage
+           document.location.replace("./index.html");
         }
     });
 };
@@ -79,4 +94,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
